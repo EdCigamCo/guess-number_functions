f21b0c776fcb33f05a0419a4125e71d71fce4967
Mister_Magic <mantana2098@gmail.com>
2026-01-09 21:07:55 +0300

    Refactoring user input and hints

--- main.go
+++ main.go
@@ -7,7 +7,6 @@ import (
 
 func main() {
 	secretNumber := rand.Intn(11)
-	var userGuess int
 
 	attempts := 3
 
@@ -18,30 +17,46 @@ func main() {
 	for attempts > 0 {
 		fmt.Printf("\nПопыток осталось: %d. Ваше число: ", attempts)
 
-		_, err := fmt.Scanln(&userGuess)
+		userGuess, err := readUserInput()
 		if err != nil {
-			fmt.Println("Ошибка чтения ввода. Пожалуйста, введите одно целое число от 0 до 10.")
-			continue
-		}
-
-		if userGuess < 0 || userGuess > 10 {
-			fmt.Println("❌ Ошибка! Число должно быть от 0 до 10")
+			fmt.Printf("Ошибка: %v\n", err)
 			continue
 		}
 
 		if userGuess == secretNumber {
 			fmt.Println("🎉 Поздравляем! Вы угадали!")
 			break
-		} else if userGuess > secretNumber {
-			fmt.Printf("Неверно! Загаданное число меньше чем %d\n", userGuess)
-		} else {
-			fmt.Printf("Неверно! Загаданное число больше чем %d\n", userGuess)
 		}
+
+		giveHint(userGuess, secretNumber)
 
 		attempts--
 	}
 
 	if attempts <= 0 {
 		fmt.Printf("\n💔 Вы проиграли! Загаданное число было: %d\n", secretNumber)
+	}
+}
+
+func readUserInput() (int, error) {
+	var userGuess int
+	_, err := fmt.Scanln(&userGuess)
+
+	if err != nil {
+		return 0, fmt.Errorf("нужно ввести целое число")
+	}
+
+	if userGuess < 0 || userGuess > 10 {
+		return 0, fmt.Errorf("число должно быть от 0 до 10")
+	}
+
+	return userGuess, nil
+}
+
+func giveHint(guess, secret int) {
+	if guess > secret {
+		fmt.Printf("Неверно! Загаданное число меньше чем %d\n", guess)
+	} else {
+		fmt.Printf("Неверно! Загаданное число больше чем %d\n", guess)
 	}
 }
